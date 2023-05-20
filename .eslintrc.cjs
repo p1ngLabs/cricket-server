7f312c25730e730df68bf2b4bbe73a7d992938b0
phuc1nguyen <phuc.ng13988@gmail.com>
2023-05-21 00:11:51 +0700

    Setup eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 'space'],
+    'linebreak-style': ['warn', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
